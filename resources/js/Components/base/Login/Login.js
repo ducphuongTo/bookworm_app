22c06311785c6621be2c7414630c1af539e6c499
ToPhuong <toducphuong7900@gmail.com>
2022-07-03 23:49:08 +0700

    feat(FE): Build login page

--- resources/js/Components/base/Login/Login.js
+++ resources/js/Components/base/Login/Login.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import { Container, Col, Row, Form, Button } from 'react-bootstrap'
+import styled from 'styled-components';
+import "./Login.css"
+import {MdClose} from 'react-icons/md'
 
-function Login() {
+export const Login = ({showLogin,setShowLogin}) => {
   return (
-    <div>Login</div>
+    <>
+      {showLogin ? (
+
+            <ModalWrapper showLogin={showLogin}>
+                  <div>
+              <Container className='mt-5'>
+                  <Row>
+                    <Col lg={4} md = {6} sm={12} className='text-center'>
+                      <img className='icon-svg' src={"http://localhost:8000/images/bookcover/user.svg"}/>
+                      <Form>
+                          <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                          
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                          </Form.Group>
+                        
+                          <Button variant="primary" type="submit">
+                            Submit
+                          </Button>
+                      </Form>
+                    </Col>
+
+                    <Col lg={8} md = {6} sm={12}>
+                      
+                    </Col>
+                  </Row>
+              </Container>
+
+          </div>
+            <CloseModalButton aria-label='Close modal' onClick={()=>setShowLogin(prev=>!prev)}/>
+            </ModalWrapper>
+
+      ):null}
+    </>
   )
 }
 
-export default Login
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0,0,0,0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+
+`
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+
+`
