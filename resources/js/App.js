c729ae3ba1cd86154f109815600a3d2ffdc021dc
ToPhuong <toducphuong7900@gmail.com>
2022-07-03 23:47:51 +0700

    feat(FE): Build header and active navlink

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,29 +1,24 @@
-import React, { Component } from "react";
-import "../css/app.css";
-
 import Home from "./pages/Home/Home";
 import Header from "./Components/base/Header/Header";
-import {Routes, Route,Link,BrowserRouter as Router} from 'react-router-dom'
-import ShopPage from "./Components/base/ShopPage/ShopPage";
+import { Routes, Route } from "react-router-dom";
+import ShopPage from "./pages/ShopPage/ShopPage";
 import About from "./pages/About/About";
-import Cart from "./pages/Cart/Cart"
+import Cart from "./pages/Cart/Cart";
+import Detail from "./pages/Detail/Detail"
+import "../css/app.css";
 
 function App() {
     return (
         <div className="App">
-        <Header/>
-         <Router>
-            
+            <Header />
             <Routes>
-                <Route exact path='/' element={<Home/>} active></Route>
-                <Route  path='/shop' element={<ShopPage/>}></Route>
-                <Route  path='/about' element={<About/>}></Route>
-                <Route  path='/about' element={<Cart/>}></Route>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/books/:id" element={<Detail />}></Route>
+                <Route path="/shop" element={<ShopPage />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/cart" element={<Cart />}></Route>
             </Routes>
-         </Router>
-            
         </div>
-
-    )
+    );
 }
 export default App;
--- resources/js/Components/base/Header/Header.js
+++ resources/js/Components/base/Header/Header.js
@@ -1,93 +1,117 @@
-import React from 'react'
-import styled from 'styled-components';
-
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
+import "./header.css"
 
 function Header() {
-    
-  return (
-    <Nav>
-        <Logo src = ""/>
-        <NavMenu>
-            <a>
-                
-                <span>HOME</span>
-            </a>
+    const [showLogin, setShowLogin] = useState(false);
 
-            <a>
+    const openModal = () => {
+        setShowLogin((prev) => !prev);
+    };
+
+    return (
+        <Nav>
+            <NavLink to="/">
+                {" "}
+                <Logo src="http://localhost:8000/images/bookcover/BOOKWORM-logo.png" />{" "}
+            </NavLink>
+            <NavMenu>
+                <NavLink to="/" style={{ textDecoration: "none", color: "white" }}
+                                className={({ isActive }) => 
+                                (isActive ? "link-active" : "link")}
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="/shop"
+                    style={{ textDecoration: "none", color: "white" }}
+                    className={({ isActive }) => 
+                      (isActive ? "link-active" : "link")}
+                >
+                    Shop
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    style={{ textDecoration: "none", color: "white" }}
+                    className={({ isActive }) => 
+                      (isActive ? "link-active" : "link")}
+                >
+                    About
+                </NavLink>
                 
-                <span>SHOP</span>
-            </a>
-            
-            <a>
-                
-                <span>ABOUT</span>
-            </a>
-            <a>
-                
-                <span>CART</span>
-            </a>
-            <a>
-                
-                <span>LOGIN</span>
-            </a>
-        </NavMenu>  
-    </Nav>
-  )
+                <NavLink
+                    to="/cart"
+                    style={{ textDecoration: "none", color: "white" }}
+                    className={({ isActive }) => 
+                      (isActive ? "link-active" : "link")}
+                >
+                    Cart(0)
+                </NavLink>
+                <NavLink
+                    to="/login"
+                    style={{ textDecoration: "none", color: "white" }}
+                >
+                    Log in
+                </NavLink>
+            </NavMenu>
+        </Nav>
+    );
 }
 
-export default Header
+export default Header;
 
-const Nav = styled.nav `
+const Nav = styled.nav`
     height: 70px;
     background-color: #090b13;
     display: flex;
     align-items: center;
     padding: 0 36px;
-    overflow-x:hidden;
-`
+    overflow-x: hidden;
+`;
 
 const Logo = styled.img`
-    width: 80px;
-`
+    width: 380px;
+`;
 
 const NavMenu = styled.div`
-    display:flex;
-    flex:1;
+    display: flex;
+    flex: 1;
     justify-content: right;
-    align-items:center;
-    a{
+    align-items: center;
+    a {
         display: flex;
-        align-items:center;
+        align-items: center;
         padding: 0 12px;
         cursor: pointer;
-        img{
+        img {
             height: 20px;
         }
-        span{
+        span {
             font-size: 13px;
             letter-spacing: 1.42px;
             position: relative;
             color: white;
-            &:after{
-                content:"";
-                height:2px;
+            &:after {
+                content: "";
+                height: 2px;
                 background: white;
-                position:absolute;
-                
-                left:0;
-                right:0;
+                position: absolute;
+
+                left: 0;
+                right: 0;
                 bottom: -6px;
-                opacity:0;
+                opacity: 0;
                 transform-origin: left center;
-                transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
                 transform: scaleX(0);
             }
         }
-        &:hover{
-            span:after{
+        &:hover {
+            span:after {
                 transform: scaleX(1);
                 opacity: 1;
             }
         }
     }
-`
+`;
