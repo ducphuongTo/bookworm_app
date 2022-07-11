d861c950b7cefaced87afef288464384492ed688
ToPhuong <toducphuong7900@gmail.com>
2022-07-11 20:44:17 +0700

    fix(fe): fix css all page

--- resources/js/pages/About/About.js
+++ resources/js/pages/About/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./about.css";
 
 export default function About() {
   return (
-    <div className="container">
+    <div className="container footer_about">
       <h3 className="fs-4 fw-bold d-inline">About Us</h3>
       <hr className='mt-4 mb-5'></hr>
       <div className="row">
@@ -15,8 +15,8 @@ export default function About() {
         <div className='col-md-2'></div>
       </div>
       <div className="row">
-        <div className='col-md-2'></div>
-        <div className="col-md-4">
+        
+        <div className="col-md-6">
           <div>
             <h2>Our Story</h2>
             <p className='mt-4'>The name Bookworm was taken from the original name for New York International Airport, which was renamed JFK in December 1963.</p>
@@ -24,15 +24,16 @@ export default function About() {
             <p>From March 2008 through May 2016, the store was located in the Flatiron District.</p>
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-6">
           <div>
             <h2>Our Vision</h2>
             <p className='mt-4'>One of the last travel bookstores in the country, our Manhattan store carries a range of guidebooks (all 10% off) to suit the needs and tastes of every traveler and budget.</p>
             <p>We believe that a novel or travelogue can be just as valuable a key to a place as any guidebook, and our well-read, well-traveled staff is happy to make reading recommendations for any traveler, book lover, or gift giver.</p>
           </div>
         </div>
-        <div className='col-md-2'></div>
+        
       </div>
+      
     </div>
   )
 }
