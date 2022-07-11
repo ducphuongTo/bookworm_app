e62270c99df97c2188e98d9b8777adcc7822bdce
ToPhuong <toducphuong7900@gmail.com>
2022-07-11 20:48:55 +0700

    fix(fe): Redirect to new tab when click book img in cart

--- resources/js/pages/Detail/Detail.js
+++ resources/js/pages/Detail/Detail.js
@@ -12,15 +12,12 @@ import {
 import { useParams } from "react-router-dom";
 import axios from "Axios";
 import "./Detail.css";
-import { QuantityPicker } from "react-qty-picker";
-import swal from 'sweetalert';
-import moment from 'moment';
-
-
+import swal from "sweetalert";
+import moment from "moment";
+import Axios from "axios";
 
 function Detail() {
     let { id } = useParams();
-
     const [book, setBook] = useState([]);
     const [bookReview, setBookReview] = useState({
         reviewTotal: 0,
@@ -29,55 +26,102 @@ function Detail() {
         reviewCountStart: [],
         sort: "",
     });
-
-    const [reviewTitle,setReviewTitle] = useState('')
-    const [reviewDetails,setReviewDetails] = useState('')
+    const [reviewTitle, setReviewTitle] = useState("");
+    const [reviewDetails, setReviewDetails] = useState("");
     const [reviewStar, setReviewStar] = useState({
-        reviewStar: 1
+        reviewStar: 1,
     });
     const handleChangeReviewStar = (e) => {
-        setReviewStar(prevStateStar => ({
-            ...prevStateStar, reviewStar: e.target.value
+        setReviewStar((prevStateStar) => ({
+            ...prevStateStar,
+            reviewStar: e.target.value,
         }));
-        
     };
-    const [cartItem,setCartItem] = useState([])
-    
-
-    const handleAddToCart = (e)=>{
-        setCartItem({cartItem:book})
-          
-          console.log(cartItem)
+    const [stateQty, setStateQty] = useState(1);
+    const handleIncreaseQuantity = (e) =>{
+        setStateQty(stateQty + 1 <= 8 ? stateQty + 1 : 8)
+    }
+    const handleDecreaseQty = (e)=>{
+        setStateQty(stateQty - 1 >=1 ? stateQty -1 : 1)
     }
 
-   
+
+    const handleAddToCart = (e) => {
+        const quantity = stateQty;
+        let cart = [];
+        let cartItems = {};
+        let total_cart = 0;
+        let totalCartPrice;
+        
+        
+        if (localStorage.getItem("cart")) {
+            cart = JSON.parse(localStorage.getItem("cart"));
+        }
+        if (localStorage.getItem("total_cart")) {
+            total_cart = JSON.parse(localStorage.getItem("total_cart"));
+        }
+        if(localStorage.getItem("totalCartPrice"))
+        {
+            totalCartPrice = JSON.parse(localStorage.getItem("totalCartPrice"))
+        }
+        let idBook = id;
+        const findIndex = cart.findIndex(
+            (e) => e.id.toString() === idBook.toString()
+        );
+        if (findIndex !== -1) {
+            cart[findIndex].quantity += quantity;
+            total_cart += quantity;
+            totalCartPrice = cart.reduce((acc,item)=>acc + item.final_price * item.quantity,0)
+        } else {
+            cartItems = {
+                id: book[0].id,
+                author_id: book[0].author_id,
+                book_title: book[0].book_title,
+                author_name: book[0].author_name,
+                book_price: book[0].book_price,
+                book_cover_photo: book[0].book_cover_photo,
+                discount_price: book[0].book_price,
+                final_price: book[0].final_price,
+                average_start: book[0].average_start,
+                quantity: quantity,
+                
+            };
+            cart.push(cartItems);
+            total_cart+=quantity;
+            totalCartPrice = cart.reduce((acc,item)=>acc + item.final_price * item.quantity,0)
+            
+        }
+        localStorage.setItem("cart", JSON.stringify(cart));
+        localStorage.setItem("total_cart", JSON.stringify(total_cart));
+        localStorage.setItem("totalCartPrice", JSON.stringify(totalCartPrice));
+    };
+
     useEffect(() => {
         getBookData();
         getReviewData();
     }, []);
 
+    const initial_url = `http://127.0.0.1:8000/api/review/sort/${id}`;
+    const [per_page, setPerPage] = useState(5);
+    const [current_sort, setCurrentSort] = useState("sort[review_date]=desc");
+    const [current_sort_display, setCurrentSortDisplay] =
+        useState("Sort by newest to oldest");
     
-
-    const handleSortData = async (e) => {
-        let url = `http://127.0.0.1:8000/api/review/${id}`;
-        let arr = [];
-       
-        arr.push(`${e}`);
-        for (let i = 0; i < arr.length; i++) {
-            if (i === 0) {
-                url += "?" + arr[i];
-            } else {
-                url += "&" + arr[i];
+        const handleSortData = async ({
+            page = undefined,
+            perPage = per_page,
+            sort = current_sort,
+            
+        } = {}) => {
+            let url = `${initial_url}?${sort}&paginate=${perPage}`;
+            if (page > 1) {
+                url += `&page=${page}`;
             }
-        }
-       
-        axios.get(url).then((res) => {
-            const data = res.data;
-           
-            setBookReview({ reviewList: data });
-        });
-        
-    };
+            const response = await Axios.get(url);
+            setBook({
+                book: response.data.data.data,
+            });
+        };
 
     const getReviewData = async () => {
         const reviewListData = await axios.get(
@@ -113,23 +157,28 @@ function Detail() {
                 }
             );
     };
-    const handleSubmitPreview = async () =>{
-        const resData = await axios
-                                .post('http://127.0.0.1:8000/api/review/book',{
-            book_id:id,
-            review_title: reviewTitle,
-            review_details: reviewDetails,
-            rating_start: reviewStar.reviewStar
-        })
-        if(resData.data.success){
-            getReviewData()
-            swal("Page will reload after 5 seconds",resData.data.message,"success")
-            setTimeout(()=>{
-                
-                    location.reload()
-            },5000)
+    const handleSubmitPreview = async () => {
+        const resData = await axios.post(
+            "http://127.0.0.1:8000/api/review/book",
+            {
+                book_id: id,
+                review_title: reviewTitle,
+                review_details: reviewDetails,
+                rating_start: reviewStar.reviewStar,
+            }
+        );
+        if (resData.data.success) {
+            getReviewData();
+            swal(
+                "Page will reload after 5 seconds",
+                resData.data.message,
+                "success"
+            );
+            setTimeout(() => {
+                location.reload();
+            }, 5000);
         }
-    }
+    };
     return (
         <div className="container">
             {book.length > 0 &&
@@ -142,15 +191,16 @@ function Detail() {
                         <div className="row">
                             <div className="col-md-8">
                                 <Card className="book-detail">
-                                    <div className="col-md-6 pl-1">
+                                    <div className="col-md-6">
                                         <Card.Img
                                             src={
                                                 "http://127.0.0.1:8000/images/bookcover/" +
                                                 item.book_cover_photo +
                                                 ".jpg"
                                             }
+                                            className="img-detail"
                                         />
-                                        <Card.Text>
+                                        <Card.Text className="text-detail">
                                             By(author){" "}
                                             <strong>{item.author_name}</strong>
                                         </Card.Text>
@@ -177,16 +227,33 @@ function Detail() {
                                         </del>{" "}
                                         <strong>${item.final_price}</strong>
                                     </Card.Footer>
-                                    <Card.Text className="qty">
-                                        Quantity
-                                    </Card.Text>
+
                                     <div className="px-5">
-                                        <QuantityPicker
-                                            min={1}
-                                            max={8}
-                                            default={1}
-                                            
-                                        />
+                                        <Card.Text className="qty">
+                                            Quantity
+                                        </Card.Text>
+                                        <div className="bm-flex bg-quantity text-white height-quantity control_qty">
+
+                                                <button
+                                                    type="button"
+                                                    className="btn  text-white btn_qty"
+                                                    onClick={handleDecreaseQty}
+                                                >
+                                                    -
+                                                </button>
+                                            <div className="item_qty">
+                                                {stateQty}
+                                            </div>
+
+                                                <button
+                                                    type="button"
+                                                    className="btn text-white btn_qty"
+                                                    onClick={handleIncreaseQuantity}
+                                                >
+                                                    +
+                                                </button>
+
+                                        </div>
                                     </div>
                                     <div className="d-grid gap-2 addToCart">
                                         <Button
@@ -220,22 +287,28 @@ function Detail() {
                             <div className="d-flex">
                                 <div>
                                     <Card.Text>
-                                        <strong>({bookReview.reviewTotal})</strong>
+                                        <strong>
+                                            ({bookReview.reviewTotal})
+                                        </strong>
                                     </Card.Text>
                                 </div>
-                                { 
-                                bookReview.reviewCountStart.length > 0 &&
-                                bookReview.reviewCountStart.map((item, i) => (
-                                    <div key={i}>
-                                        <Card.Text
-                                            style={{ textDecoration: "underline" }}
-                                        >
-                                            {" "}
-                                            {item.rating_start} start ({item.count})
-                                            |{" "}
-                                        </Card.Text>
-                                    </div>
-                                ))}
+                                {bookReview.reviewCountStart.length > 0 &&
+                                    bookReview.reviewCountStart.map(
+                                        (item, i) => (
+                                            <div key={i}>
+                                                <Card.Text
+                                                    style={{
+                                                        textDecoration:
+                                                            "underline",
+                                                    }}
+                                                >
+                                                    {" "}
+                                                    {item.rating_start} start (
+                                                    {item.count}) |{" "}
+                                                </Card.Text>
+                                            </div>
+                                        )
+                                    )}
                             </div>
                         </div>
 
@@ -280,7 +353,11 @@ function Detail() {
                                             {item.review_details}
                                         </Card.Text>
                                         <Card.Text>
-                                            <strong>{moment(item.review_date).format("LL")}</strong>
+                                            <strong>
+                                                {moment(
+                                                    item.review_date
+                                                ).format("LL")}
+                                            </strong>
                                         </Card.Text>
                                         <hr className="mt-5"></hr>
                                     </div>
@@ -299,7 +376,7 @@ function Detail() {
                             <Form.Control
                                 type="text"
                                 placeholder="Add title here!"
-                                onChange={e => setReviewTitle(e.target.value)}
+                                onChange={(e) => setReviewTitle(e.target.value)}
                             />
                         </Form.Group>
 
@@ -313,7 +390,9 @@ function Detail() {
                             <Form.Control
                                 type="text"
                                 placeholder="Details go here"
-                                onChange={e => setReviewDetails(e.target.value)}
+                                onChange={(e) =>
+                                    setReviewDetails(e.target.value)
+                                }
                             />
                         </Form.Group>
 
@@ -322,16 +401,26 @@ function Detail() {
                             controlId="formBasicPassword"
                         >
                             <Form.Label>Select a rating start</Form.Label>
-                            
                         </Form.Group>
-                        <select className="dropdown btn bg-quantity "
-                                onChange={handleChangeReviewStar}
+                        <select
+                            className="dropdown btn bg-quantity "
+                            onChange={handleChangeReviewStar}
                         >
-                                    <option value={1} className='bg-quantity'>1 Star</option>
-                                    <option value={2} className='bg-quantity'>2 Star</option>
-                                    <option value={3} className='bg-quantity'>3 Star</option>
-                                    <option value={4} className='bg-quantity'>4 Star</option>
-                                    <option value={5} className='bg-quantity'>5 Star</option>
+                            <option value={1} className="bg-quantity">
+                                1 Star
+                            </option>
+                            <option value={2} className="bg-quantity">
+                                2 Star
+                            </option>
+                            <option value={3} className="bg-quantity">
+                                3 Star
+                            </option>
+                            <option value={4} className="bg-quantity">
+                                4 Star
+                            </option>
+                            <option value={5} className="bg-quantity">
+                                5 Star
+                            </option>
                         </select>
                         <hr className="mt-4"></hr>
                         <Button
